19273b7b51335917239eeef5cb5a3e3259fd778b
Peacemaker <devpeacemd@gmail.com>
2026-07-10 17:16:36 +0300

    Refactor SettingsProvider to use SharedPreferences

--- kjv.js
+++ kjv.js
@@ -1,59 +1,34 @@
 import 'package:flutter/material.dart';
-import '../services/settings_service.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-class SettingsProvider extends ChangeNotifier {
-  ThemeMode _themeMode = ThemeMode.system;
-  double _fontSize = 18;
-
-  bool _dailyVerse = true;
-  bool _soundEnabled = true;
-  bool _keepScreenOn = false;
-
-  ThemeMode get themeMode => _themeMode;
-  double get fontSize => _fontSize;
-
-  bool get dailyVerse => _dailyVerse;
-  bool get soundEnabled => _soundEnabled;
-  bool get keepScreenOn => _keepScreenOn;
-
-  Future<void> load() async {
-    _themeMode = await SettingsService.getThemeMode();
-    _fontSize = await SettingsService.getFontSize();
-
-    _dailyVerse = await SettingsService.getDailyVerse();
-    _soundEnabled = await SettingsService.getSoundEnabled();
-    _keepScreenOn = await SettingsService.getKeepScreenOn();
-
-    notifyListeners();
+class SettingsService {
+  // Theme
+  static Future<void> saveThemeMode(ThemeMode mode) async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setString("theme", mode.name);
   }
 
-  Future<void> setThemeMode(ThemeMode mode) async {
-    _themeMode = mode;
-    await SettingsService.saveThemeMode(mode);
-    notifyListeners();
+  static Future<ThemeMode> getThemeMode() async {
+    final prefs = await SharedPreferences.getInstance();
+
+    switch (prefs.getString("theme")) {
+      case "light":
+        return ThemeMode.light;
+      case "dark":
+        return ThemeMode.dark;
+      default:
+        return ThemeMode.system;
+    }
   }
 
-  Future<void> setFontSize(double size) async {
-    _fontSize = size;
-    await SettingsService.saveFontSize(size);
-    notifyListeners();
+  // Font Size
+  static Future<void> saveFontSize(double size) async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setDouble("fontSize", size);
   }
 
-  Future<void> setDailyVerse(bool value) async {
-    _dailyVerse = value;
-    await SettingsService.saveDailyVerse(value);
-    notifyListeners();
-  }
-
-  Future<void> setSoundEnabled(bool value) async {
-    _soundEnabled = value;
-    await SettingsService.saveSoundEnabled(value);
-    notifyListeners();
-  }
-
-  Future<void> setKeepScreenOn(bool value) async {
-    _keepScreenOn = value;
-    await SettingsService.saveKeepScreenOn(value);
-    notifyListeners();
+  static Future<double> getFontSize() async {
+    final prefs = await SharedPreferences.getInstance();
+    return prefs.getDouble("fontSize") ?? 18;
   }
 }
