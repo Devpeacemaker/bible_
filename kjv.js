1f20d58167da1da9bb2af83753f8e2fa15b92723
Peacemaker <devpeacemd@gmail.com>
2026-07-15 22:41:11 +0300

    Refactor ApiConstants and remove AudioService
    Removed AudioService class and its method for fetching audio Bibles.

--- kjv.js
+++ kjv.js
@@ -1,41 +1,7 @@
-import 'dart:convert';
-import 'package:http/http.dart' as http;
+class ApiConstants {
+  static const String apiKey =
+      "y6l2BI1FRgTP8oclOGoAU";
 
-import '../constants/api_constants.dart';
-
-
-class AudioService {
-
-  static Future<List<dynamic>> getAudioBibles() async {
-
-    final response = await http.get(
-
-      Uri.parse(
-        "${ApiConstants.baseUrl}/audio-bibles",
-      ),
-
-      headers: {
-
-        "api-key": ApiConstants.apiKey,
-
-      },
-
-    );
-
-
-    if (response.statusCode == 200) {
-
-      final data =
-          jsonDecode(response.body);
-
-      return data["data"];
-
-    } else {
-
-      throw Exception(
-        "Failed to load audio Bibles",
-      );
-
-    }
-  }
+  static const String baseUrl =
+      "https://rest.api.bible/v1";
 }
