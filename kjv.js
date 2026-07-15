a3c0815eb3abf7121b86c0da3de733c855df3127
Peacemaker <devpeacemd@gmail.com>
2026-07-15 17:04:44 +0300

    Update kjv.js

--- kjv.js
+++ kjv.js
@@ -1,39 +1,34 @@
+name: peace_m_bible
+description: "A new Flutter project."
+publish_to: 'none'
 
-import 'dart:convert';
-import 'package:http/http.dart' as http;
+version: 1.0.0+1
 
-class AIService {
-  static const String apiKey = "AQ.Ab8RN6IZoVVsrvaLFF4YIM4pzrAtr_-bGizHVTEyh_P1sqfPsw";
+environment:
+  sdk: ^3.10.8
 
-  static Future<String> askBible(String question) async {
-    final url = Uri.parse(
-      "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=$apiKey",
-    );
+dependencies:
+  flutter:
+    sdk: flutter
 
-    final response = await http.post(
-      url,
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: jsonEncode({
-        "contents": [
-          {
-            "parts": [
-              {
-                "text":
-                    "You are PEACE M Bible AI. Answer only using biblical teachings. Include Bible verses where appropriate. Question: $question"
-              }
-            ]
-          }
-        ]
-      }),
-    );
 
-    if (response.statusCode == 200) {
-      final data = jsonDecode(response.body);
-      return data["candidates"][0]["content"]["parts"][0]["text"];
-    }
 
-    return "Failed to get AI response.";
-  }
-}
+  cupertino_icons: ^1.0.8
+  shared_preferences: ^2.2.3
+  uuid: ^4.5.1
+  share_plus: ^11.0.0
+  provider: ^6.1.5
+  http: ^1.2.1
+  hive: ^2.2.3
+  hive_flutter: ^1.1.0
+dev_dependencies:
+  flutter_test:
+    sdk: flutter
+
+  flutter_lints: ^6.0.0
+
+flutter:
+  uses-material-design: true
+
+  assets:
+    - assets/bibles/
