057e0a997fe805a0d467a1cbc9e269e66254dd31
Peacemaker <devpeacemd@gmail.com>
2026-07-16 23:08:17 +0300

    Refactor AudioService into AudioBibleScreen

--- peace.js
+++ peace.js
@@ -1,207 +1,277 @@
-import 'dart:convert';
-import 'package:http/http.dart' as http;
+import 'package:flutter/material.dart';
 
-import '../constants/api_constants.dart';
-
-
-class AudioService {
+import '../services/audio_service.dart';
+import 'audio_books_screen.dart';
 
 
-  // Get available Audio Bibles
-  static Future<List<dynamic>> getAudioBibles() async {
+class AudioBibleScreen extends StatefulWidget {
 
-    final response = await http.get(
-
-      Uri.parse(
-        "${ApiConstants.baseUrl}/audio-bibles",
-      ),
-
-      headers: {
-
-        "api-key":
-            ApiConstants.apiKey,
-
-      },
-
-    );
+  const AudioBibleScreen({super.key});
 
 
-    if (response.statusCode == 200) {
+  @override
+  State<AudioBibleScreen> createState() =>
+      _AudioBibleScreenState();
 
-      final data =
-          jsonDecode(response.body);
-
-      return data["data"];
-
-    }
+}
 
 
-    throw Exception(
-      "Unable to load audio Bibles",
-    );
+
+class _AudioBibleScreenState
+    extends State<AudioBibleScreen> {
+
+
+  bool loading = true;
+
+  List<dynamic> audioBibles = [];
+
+
+
+  @override
+  void initState(){
+
+    super.initState();
+
+    loadAudioBibles();
 
   }
 
 
 
+  Future<void> loadAudioBibles() async {
 
-  // Get books inside selected Audio Bible
-  static Future<List<dynamic>> getBooks({
+    try {
 
-    required String bibleId,
-
-  }) async {
-
-
-    final response = await http.get(
-
-      Uri.parse(
-
-        "${ApiConstants.baseUrl}/audio-bibles/"
-        "$bibleId/books",
-
-      ),
+      final result =
+          await AudioService.getAudioBibles();
 
 
-      headers: {
+      setState((){
 
-        "api-key":
-            ApiConstants.apiKey,
+        audioBibles = result;
 
-      },
+        loading = false;
 
-    );
-
+      });
 
 
-    if(response.statusCode == 200){
+    } catch(e){
+
+      setState((){
+
+        loading = false;
+
+      });
 
 
-      final data =
-          jsonDecode(response.body);
+      if(mounted){
 
+        ScaffoldMessenger.of(context)
+            .showSnackBar(
 
-      return data["data"];
+          SnackBar(
+
+            content:
+                Text(
+                  e.toString(),
+                ),
+
+          ),
+
+        );
+
+      }
 
     }
-
-
-
-    throw Exception(
-      "Unable to load Bible books",
-    );
 
   }
 
 
 
-
-  // Get chapters inside selected book
-  static Future<List<dynamic>> getBookChapters({
-
-    required String bibleId,
-
-    required String bookId,
-
-  }) async {
+  @override
+  Widget build(BuildContext context){
 
 
+    return Scaffold(
 
-    final response = await http.get(
+      appBar: AppBar(
 
-      Uri.parse(
+        title:
+            const Text(
+              "Audio Bible",
+            ),
 
-        "${ApiConstants.baseUrl}/audio-bibles/"
-        "$bibleId/books/$bookId/chapters",
+        centerTitle:true,
 
       ),
 
 
-      headers: {
 
-        "api-key":
-            ApiConstants.apiKey,
-
-      },
-
-    );
+      body: loading
 
 
+          ? const Center(
 
-    if(response.statusCode == 200){
+              child:
+                  CircularProgressIndicator(),
 
-
-      final data =
-          jsonDecode(response.body);
-
-
-      return data["data"];
-
-    }
+            )
 
 
 
-    throw Exception(
-      "Unable to load chapters",
+          : audioBibles.isEmpty
+
+
+              ? const Center(
+
+                  child:
+                      Text(
+                        "No Audio Bibles available",
+                      ),
+
+                )
+
+
+
+              : ListView.builder(
+
+                  padding:
+                      const EdgeInsets.all(16),
+
+
+                  itemCount:
+                      audioBibles.length,
+
+
+
+                  itemBuilder:(context,index){
+
+
+                    final bible =
+                        audioBibles[index];
+
+
+
+                    return Card(
+
+                      elevation:4,
+
+
+                      margin:
+                          const EdgeInsets.only(
+                            bottom:12,
+                          ),
+
+
+
+                      shape:
+                          RoundedRectangleBorder(
+
+                        borderRadius:
+                            BorderRadius.circular(18),
+
+                      ),
+
+
+
+                      child:ListTile(
+
+
+                        contentPadding:
+                            const EdgeInsets.all(16),
+
+
+
+                        leading:
+                            const CircleAvatar(
+
+                          radius:25,
+
+                          child:
+                              Icon(
+                                Icons.headphones,
+                              ),
+
+                        ),
+
+
+
+                        title:
+                            Text(
+
+                              bible["name"] ??
+                                  "Audio Bible",
+
+                              style:
+                                  const TextStyle(
+
+                                fontWeight:
+                                    FontWeight.bold,
+
+                              ),
+
+                            ),
+
+
+
+                        subtitle:
+                            Text(
+
+                              bible["description"] ??
+                                  "Listen to Scripture",
+
+                            ),
+
+
+
+                        trailing:
+                            const Icon(
+
+                              Icons.arrow_forward_ios,
+
+                              size:18,
+
+                            ),
+
+
+
+                        onTap:(){
+
+
+                          Navigator.push(
+
+                            context,
+
+                            MaterialPageRoute(
+
+                              builder:(_)=>
+
+                                  AudioBooksScreen(
+
+                                bibleId:
+                                    bible["id"],
+
+                              ),
+
+                            ),
+
+                          );
+
+
+                        },
+
+
+                      ),
+
+                    );
+
+
+                  },
+
+                ),
+
     );
 
   }
-
-
-
-
-
-  // Get real audio URL for chapter
-  static Future<String> getChapterAudioUrl({
-
-    required String bibleId,
-
-    required String chapterId,
-
-  }) async {
-
-
-
-    final response = await http.get(
-
-      Uri.parse(
-
-        "${ApiConstants.baseUrl}/audio-bibles/"
-        "$bibleId/chapters/$chapterId",
-
-      ),
-
-
-      headers: {
-
-        "api-key":
-            ApiConstants.apiKey,
-
-      },
-
-    );
-
-
-
-    if(response.statusCode == 200){
-
-
-      final data =
-          jsonDecode(response.body);
-
-
-
-      return data["data"]["resourceUrl"];
-
-    }
-
-
-
-    throw Exception(
-      "Unable to load chapter audio",
-    );
-
-  }
-
 
 }
