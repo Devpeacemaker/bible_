ee62d930c2ecd30743ff8a552983239542245d6b
Peacemaker <devpeacemd@gmail.com>
2026-07-17 21:00:52 +0300

    Implement SwahiliBibleService to load Bible data

--- peace.js
+++ peace.js
@@ -1,1 +1,20 @@
-f
+import 'dart:convert';
+
+import 'package:flutter/services.dart';
+
+class SwahiliBibleService {
+  static List _books = [];
+
+  static Future<void> loadBible() async {
+    final jsonString =
+        await rootBundle.loadString(
+      'assets/bibles/swa.json',
+    );
+
+    _books = json.decode(jsonString);
+  }
+
+  static List getBooks() {
+    return _books;
+  }
+}
