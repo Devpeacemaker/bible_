722d09f5751a3b1586c8b6a404d6c000eff72037
Peacemaker <devpeacemd@gmail.com>
2026-07-18 12:28:17 +0300

    Update peace.js

--- peace.js
+++ peace.js
@@ -1,38 +1,45 @@
-Card(
-  elevation: 6,
+<manifest xmlns:android="http://schemas.android.com/apk/res/android">
+    <application
+        android:label="Peace M Bible"
+        android:name="${applicationName}"
+        android:icon="@mipmap/ic_launcher">
+        <activity
+            android:name=".MainActivity"
+            android:exported="true"
+            android:launchMode="singleTop"
+            android:taskAffinity=""
+            android:theme="@style/LaunchTheme"
+            android:configChanges="orientation|keyboardHidden|keyboard|screenSize|smallestScreenSize|locale|layoutDirection|fontScale|screenLayout|density|uiMode"
+            android:hardwareAccelerated="true"
+            android:windowSoftInputMode="adjustResize">
+            <!-- Specifies an Android theme to apply to this Activity as soon as
+                 the Android process has started. This theme is visible to the user
+                 while the Flutter UI initializes. After that, this theme continues
+                 to determine the Window background behind the Flutter UI. -->
+            <meta-data
+              android:name="io.flutter.embedding.android.NormalTheme"
+              android:resource="@style/NormalTheme"
+              />
+            <intent-filter>
+                <action android:name="android.intent.action.MAIN"/>
+                <category android:name="android.intent.category.LAUNCHER"/>
+            </intent-filter>
+        </activity>
+        <!-- Don't delete the meta-data below.
+             This is used by the Flutter tool to generate GeneratedPluginRegistrant.java -->
+        <meta-data
+            android:name="flutterEmbedding"
+            android:value="2" />
+    </application>
+    <!-- Required to query activities that can process text, see:
+         https://developer.android.com/training/package-visibility and
+         https://developer.android.com/reference/android/content/Intent#ACTION_PROCESS_TEXT.
 
-  shape: RoundedRectangleBorder(
-    borderRadius: BorderRadius.circular(20),
-  ),
-
-  child: ListTile(
-
-    leading: const CircleAvatar(
-      backgroundColor: Colors.green,
-
-      child: Padding(
-        padding: EdgeInsets.all(8),
-        child: Image(
-          image: AssetImage(
-            "assets/images/whatsapp.png",
-          ),
-        ),
-      ),
-    ),
-
-    title: const Text(
-      "Contact Support",
-    ),
-
-    subtitle: const Text(
-      "Get help through WhatsApp",
-    ),
-
-    trailing: const Icon(
-      Icons.open_in_new,
-    ),
-
-    onTap: _openWhatsApp,
-
-  ),
-),
+         In particular, this is used by the Flutter engine in io.flutter.plugin.text.ProcessTextPlugin. -->
+    <queries>
+        <intent>
+            <action android:name="android.intent.action.PROCESS_TEXT"/>
+            <data android:mimeType="text/plain"/>
+        </intent>
+    </queries>
+</manifest>
