1bc2d74742d148824331669872fc8b4b7998ff0f
Peacemaker <devpeacemd@gmail.com>
2026-07-18 15:58:59 +0300

    Fix keytool path in peace.js

--- peace.js
+++ peace.js
@@ -1,1 +1,1 @@
-keytool -genkeypair -v -keystore peace_m_bible.jks -keyalg RSA -keysize 2048 -validity 10000 -alias peace_m_bible
+& "C:\Program Files\Android\Android Studio\jbr\bin\keytool.exe" -genkeypair -v -keystore peace_m_bible.jks -keyalg RSA -keysize 2048 -validity 10000 -alias peace_m_bible
