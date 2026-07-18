b1775ea5697b8a8547db58f563020f27e65cf102
Peacemaker <devpeacemd@gmail.com>
2026-07-18 16:08:37 +0300

    Update keytool command parameters in peace.js

--- peace.js
+++ peace.js
@@ -1,1 +1,4 @@
-& "C:\Program Files\Android\Android Studio\jbr\bin\keytool.exe" -genkeypair -v -keystore peace_m_bible.jks -keyalg RSA -keysize 2048 -validity 10000 -alias peace_m_bible
+storePassword=Peacemaker122
+keyPassword=Peacemaker122
+keyAlias=peace_m_bible
+storeFile=peace_m_bible.jks
